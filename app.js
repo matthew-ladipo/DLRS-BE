af695713db83ebd310ba74f9fd3efc734e114885
john doe <johndoe@gmail.com>
2025-09-22 21:32:58 +0100

    cor

--- app.js
+++ app.js
@@ -4,20 +4,34 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import { protect } from './middlewares/auth.middleware.js';
 
-
 // Load env
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: process.env.CLIENT_URL || "https://departmental-lecture-repository-sys.vercel.app", // your frontend origin
-  credentials: true,  // allow cookies, auth headers
-}));app.use(express.json());
+
+// ✅ CORS configuration
+app.use(
+  cors({
+    origin: [
+     process.env.CLIENT_URL?.trim(), // your frontend
+      "http://localhost:3000", // allow local dev too
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+
+
+
+app.use(express.json());
 
 // DB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 // Routes
 import authRoutes from './routes/auth.routes.js';
@@ -27,12 +41,12 @@ app.use('/api/auth', authRoutes);
 // app.use('/api/resources', resourceRoutes);
 
 app.get('/api/protected', protect, (req, res) => {
-    res.json({ message: 'Access granted!', user: req.user });
-  });
+  res.json({ message: 'Access granted!', user: req.user });
+});
 
-  app.get('/', (req, res) => {
-    res.send('Backend API is running');
-  });
+app.get('/', (req, res) => {
+  res.send('Backend API is running');
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
